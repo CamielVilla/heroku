6e4e9af37e68641434fb493ff1e508faa26aa88e
CamielVilla <camielvilla@gmail.com>
2023-09-30 18:26:23 +0200

    wagebudy email success

--- src/main/java/nl/thelastages/website/configuration/EmailConfiguration.java
+++ src/main/java/nl/thelastages/website/configuration/EmailConfiguration.java
@@ -1,22 +1,9 @@
 package nl.thelastages.website.configuration;
 
-import com.fasterxml.jackson.databind.annotation.JsonAppend;
-import jakarta.mail.*;
-import jakarta.mail.internet.MimeMessage;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMessagePreparator;
-
-import java.io.InputStream;
-import java.util.Date;
-import java.util.Properties;
 @Configuration
 public class EmailConfiguration {
     @Bean
--- src/main/java/nl/thelastages/website/exception/UserAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package nl.thelastages.website.exception;
-
-public class UserAlreadyExistException extends RuntimeException{
-    public UserAlreadyExistException(String email) {
-        super(email + " is allready in database");
-    }
-}
